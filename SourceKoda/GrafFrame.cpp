0137c426b6374c315caae1fb0768eb8cdcc7dfa6
Jan Obrovnik <jo7261@student.uni-lj.si>
2024-08-12 14:04:46 +0200

    Popravki - risanje grafov - splošni

--- SourceKoda/GrafFrame.cpp
+++ SourceKoda/GrafFrame.cpp
@@ -53,6 +53,11 @@ std::vector<std::vector<double>> izracunGraf(std::vector<std::string> seznamEnac
 
 			res[i + 1].push_back(j * j + 5);
 		}
+
+		else for (double j = 0; j <= vrednost; j += korak) {
+
+			res[i + 1].push_back(0);
+		}
 	}
 
 	(*grafX).assign({ 0., .2, .4, .6, .8, 1. }); // X os
@@ -62,8 +67,10 @@ std::vector<std::vector<double>> izracunGraf(std::vector<std::string> seznamEnac
 	double max = maxVrednost(izr);
 	double min = minVrednost(izr);
 	
-	for (int i = 0; i < (*grafX).size(); i++) (*grafX)[i] *= vrednost; // X os
+	for (int i = 0; i < (*grafX).size(); i++) (*grafX)[i] *= vrednost; // X os ///////////////////// izpise velik decimalk
 	for (int i = 0; i < (*grafY).size(); i++) (*grafY)[i] = min + i * (max - min) / ((*grafY).size() - 1); // Y osi
+
+	if ((*grafY)[0] == (*grafY)[(*grafX).size() - 1]) (*grafY).assign({ (*grafY)[0] - .5, (*grafY)[0] - .3, (*grafY)[0] - .1, (*grafY)[0] + .1, (*grafY)[0] + .3, (*grafY)[0] + .5 }); // Popravek za konst. funkcije
 
 	return res;
 }
@@ -100,24 +107,26 @@ GrafFrame::GrafFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	seznamBarv.push_back(wxPen(wxColour(102, 102, 102), 2, wxPENSTYLE_SOLID));
 	seznamBarv.push_back(wxPen(wxColour(204, 204, 204), 2, wxPENSTYLE_SOLID));
 
+
 	wxPanel* panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxWANTS_CHARS);
 	
-	wxButton* risi = new wxButton(panel, wxID_ANY, "Risi", wxPoint(10, 530), wxSize(80, 60));
-	wxButton* shraniData = new wxButton(panel, wxID_ANY, "Shrani meritve", wxPoint(640, 560), wxDefaultSize);
+	wxButton* risi = new wxButton(panel, wxID_ANY, "Risi", wxPoint(10, 475), wxSize(108, 54));
+	wxButton* shraniData = new wxButton(panel, wxID_ANY, "Shrani meritve", wxPoint(10, 535), wxSize(108, 54));
 
 	velicineY.Clear();
 	velicineY.Add("x");
 	velicineY.Add("2x");
 	velicineY.Add("x2");
-	checkListBoxYOs = new wxCheckListBox(panel, wxID_ANY, wxPoint(5, 200), wxDefaultSize, velicineY);
+	velicineY.Add("0");
+	checkListBoxYOs = new wxCheckListBox(panel, wxID_ANY, wxPoint(10, 25), wxSize(108, -1), velicineY);
 
-	ctrlVrednostMeritveX = new wxSpinCtrlDouble(panel, wxID_ANY, "", wxPoint(900, 550), wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, 0, 1000, 10, .1);
-	ctrlKorakMeritveX = new wxSpinCtrlDouble(panel, wxID_ANY, "", wxPoint(160, 550), wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, 0, 10, 1, .01);
+	ctrlVrednostMeritveX = new wxSpinCtrlDouble(panel, wxID_ANY, "", wxPoint(878, 566), wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, 0, 1000, 10, .1);
+	ctrlKorakMeritveX = new wxSpinCtrlDouble(panel, wxID_ANY, "", wxPoint(215, 566), wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, 0, 10, 1, .01);
 	
 	wxArrayString velicineX;
 	velicineX.Add("t [s]");
 	velicineX.Add("x [m]");
-	radioVelicinaX = new wxRadioBox(panel, wxID_ANY, "", wxPoint(500, 540), wxDefaultSize, velicineX, 1, wxRA_SPECIFY_ROWS);
+	radioVelicinaX = new wxRadioBox(panel, wxID_ANY, "", wxPoint(505, 555), wxDefaultSize, velicineX, 1, wxRA_SPECIFY_ROWS);
 
 
 	risi->Bind(wxEVT_BUTTON, &GrafFrame::OnButtonRisiClicked, this);
@@ -191,9 +200,12 @@ void GrafFrame::OnPaint(wxPaintEvent& event) {
 	if (seznamEnacb.size() > 0) res = izracunGraf(seznamEnacb, ctrlKorakMeritveX->GetValue(), ctrlVrednostMeritveX->GetValue(), &grafX, &grafY);
 	else res.clear();
 
+	dc.DrawText("Korak =", wxPoint(170, 567));
+	dc.DrawText("Obmocje =", wxPoint(813, 567));
+
 	//// Ozadje
-	wxPoint pointGraf(120, 20);
-	wxSize sizeGraf(size.x - 160, size.y - 100);
+	wxPoint pointGraf(150, 20);
+	wxSize sizeGraf(size.x - 190, size.y - 80);
 
 	dc.SetPen(wxPen(wxColour(255, 255, 255), 1, wxPENSTYLE_SOLID));
 	dc.DrawRectangle(pointGraf, sizeGraf);
@@ -207,28 +219,15 @@ void GrafFrame::OnPaint(wxPaintEvent& event) {
 	dc.DrawLine(wxPoint(pointGraf.x, 0), wxPoint(pointGraf.x + 5, 10));
 	dc.DrawLine(wxPoint(pointGraf.x, 0), wxPoint(pointGraf.x - 5, 10));
 
-	//// Velicine
-	dc.DrawText(radioVelicinaX->GetString(radioVelicinaX->GetSelection()), wxPoint(size.x - 32, sizeGraf.y)); // X os
-
-	dc.DrawRectangle(wxPoint(10, 10), wxSize(80, 15 * seznamEnacb.size()));// Y os
-	for (int i = 0; i < seznamEnacb.size(); i++) {
-		
-		if (i == seznamBarv.size()) i -= seznamBarv.size();
-		dc.SetPen(seznamBarv[i]);
-		dc.DrawLine(wxPoint(12, 16 + i * 15), wxPoint(24, 16 + i * 15));
-		
-		dc.DrawText(seznamEnacb[i], wxPoint(30, 8 + i * 15));
-	}
-
 	//// Vrednosti
 	dc.SetPen(wxPen(wxColour(153, 153, 153), 1, wxPENSTYLE_SOLID));
 	for (int i = 0; i < grafX.size(); i++) { // X os
-		dc.DrawText(wxString::Format("%g", grafX[i]), wxPoint(120 + i * sizeGraf.x / 5 - 3, size.y - 75)); //////////////////// preureditev 'size.x' in 'size.y' z 'pointGraf' in 'sizeGraf'
-		if (i > 0) dc.DrawLine(wxPoint(120 + i * sizeGraf.x / 5, size.y - 75), wxPoint(120 + i * sizeGraf.x / 5, 20));
+		dc.DrawText(wxString::Format("%g", grafX[i]), wxPoint(pointGraf.x + i * sizeGraf.x / 5 - 3,   pointGraf.y + sizeGraf.y + 5)); //////////////////// preureditev 'size.x' in 'size.y' z 'pointGraf' in 'sizeGraf'
+		if (i > 0) dc.DrawLine(wxPoint(pointGraf.x + i * sizeGraf.x / 5, pointGraf.y + sizeGraf.y + 5), wxPoint(pointGraf.x + i * sizeGraf.x / 5, pointGraf.y));
 	}
 	for (int i = 0; i < grafY.size(); i++) { // Y os
-		dc.DrawText(wxString::Format("%g", grafY[i]), wxPoint(120 - 20, size.y - 80 - i * (size.y - 100) / 5 - 7));
-		if (i > 0) dc.DrawLine(wxPoint(120 - 5, size.y - 80 - i * (size.y - 100) / 5), wxPoint(pointGraf.x + sizeGraf.x, size.y - 80 - i * (size.y - 100) / 5));
+		dc.DrawText(wxString::Format("%g", grafY[i]), wxPoint(pointGraf.x - 20, pointGraf.y + sizeGraf.y - i * sizeGraf.y / 5 - 7));
+		if (i > 0) dc.DrawLine(wxPoint(pointGraf.x - 5, pointGraf.y + sizeGraf.y - i * sizeGraf.y / 5), wxPoint(pointGraf.x + sizeGraf.x, pointGraf.y + sizeGraf.y - i * sizeGraf.y / 5));
 	}
 	dc.SetPen(wxPen(wxColour(0, 0, 0), 1, wxPENSTYLE_SOLID));
 
@@ -236,14 +235,16 @@ void GrafFrame::OnPaint(wxPaintEvent& event) {
 	dc.SetPen(wxPen(wxColour(0, 0, 0), 2, wxPENSTYLE_SOLID));
 	for (int i = 1; i < res.size(); i++) {
 
-		if (i - 1 == seznamBarv.size()) i -= seznamBarv.size();
-		dc.SetPen(seznamBarv[i - 1]);
+		int j = i - 1;
+		while (j >= seznamBarv.size()) j -= seznamBarv.size();
+		dc.SetPen(seznamBarv[j]);
+
 		wxPointList* seznamTock = new wxPointList();
 
 		for (int j = 0; j < res[i].size(); j++) {
 
 			wxPoint* tocka;
-			tocka = new wxPoint(120 + sizeGraf.x * res[0][j] / grafX[grafX.size() - 1], size.y - 80 - (size.y - 100) * (res[i][j] - grafY[0]) / (grafY[grafY.size() - 1] - grafY[0]));
+			tocka = new wxPoint(pointGraf.x + sizeGraf.x * res[0][j] / grafX[grafX.size() - 1], pointGraf.y + sizeGraf.y - sizeGraf.y * (res[i][j] - grafY[0]) / (grafY[grafY.size() - 1] - grafY[0]));
 			seznamTock->Append(tocka);
 		}
 
@@ -251,6 +252,24 @@ void GrafFrame::OnPaint(wxPaintEvent& event) {
 	}
 	dc.SetPen(wxPen(wxColour(0, 0, 0), 1, wxPENSTYLE_SOLID));
 
+	//// Legenda
+	dc.DrawText(radioVelicinaX->GetString(radioVelicinaX->GetSelection()), wxPoint(size.x - 32, sizeGraf.y)); // X os
+	
+	dc.DrawText("Izracunaj:", wxPoint(10, 8)); // Y os
+	
+	if (seznamEnacb.size() > 0) {
+		dc.DrawRectangle(wxPoint(pointGraf.x + 10, pointGraf.y + 16), wxSize(80, 15 * seznamEnacb.size()));
+		dc.DrawText("Legenda", wxPoint(pointGraf.x + 10, pointGraf.y - 0));
+		for (int i = 0; i < seznamEnacb.size(); i++) {
+
+			int j = i;
+			while (j >= seznamBarv.size()) j -= seznamBarv.size();
+			dc.SetPen(seznamBarv[j]);
+			dc.DrawLine(wxPoint(pointGraf.x + 12, pointGraf.y + 23 + i * 15), wxPoint(pointGraf.x + 24, pointGraf.y + 23 + i * 15));
+
+			dc.DrawText(seznamEnacb[i], wxPoint(pointGraf.x + 30, pointGraf.y + 16 + i * 15));
+		}
+	}
 
 
 
